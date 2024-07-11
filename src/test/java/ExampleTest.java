11bf51e98ef965b016a090f15a7a9196e1c7e17a
Chris-Owusu <christopher.owusu@amalitech.com>
2024-07-11 11:28:30 +0000

    using the given--then keywords

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -3,16 +3,30 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.response.Response;
+
 public class ExampleTest {
 
     @Test
     public void test_1() {
 
-        Response response = RestAssured.get("https://api.restful-api.dev/objects");
-        System.out.println("This is the output of the request: " + response);
+        Response response = get("https://api.restful-api.dev/objects");
 //        System.out.println(response.prettyPeek().getBody());
         int statusCode = response.getStatusCode();
 
         Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void test_2() {
+        baseURI = "https://api.restful-api.dev/";
+
+        given().get("/objects/7")
+                .then()
+                .statusCode(200)
+                .body("data.year", equalTo(2019));
     }
 }
